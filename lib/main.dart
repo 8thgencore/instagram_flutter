3beda1a9bdb56c90230f9f6c76cc0508d5dee638
8thgencore <axelmaker@ya.ru>
2022-04-05 17:19:51 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter/responsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter/screens/login_screen.dart';
+import 'package:instagram_flutter/utils/colors.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialise app based on platform- web or mobile
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAIu_9oRxdlP6cNiCzxVyyElvrq-PwpnJw",
+        appId: "1:184380902995:web:60605daca13fa68feec0a6",
+        messagingSenderId: "184380902995",
+        projectId: "instagram-flutter-e3503",
+        storageBucket: "instagram-flutter-e3503.appspot.com",
+        authDomain: "instagram-flutter-e3503.firebaseapp.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Instagram Clone",
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      // home: const ResponsiveLayout(
+      //   webScreenLayout: WebScreenLayout(),
+      //   mobileScreenLayout: MobileScreenLayout(),
+      // ),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            // Checking if the snapshot has any data or not
+            if (snapshot.hasData) {
+              // if snapshot has data which means user is logged in then
+              // we check the width of screen and accordingly display the screen layout
+              return const ResponsiveLayout(
+                webScreenLayout: WebScreenLayout(),
+                mobileScreenLayout: MobileScreenLayout(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: primaryColor,
+              ),
+            );
+          }
+
+          return const LoginScreen();
+        },
+      ),
+    );
+  }
+}
---
+++ lib/utils/images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  final String logo = 'assets/ic_instagram.svg';
+  final String defaultProfile = 'assets/default_profile_photo.jpg';
+}
